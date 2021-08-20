b9c384d2aa7153c20c00544ee01ae1fd3bc82182
Saarah Bookwala <76689592+SaarahBookwala@users.noreply.github.com>
2021-08-20 16:45:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+img = "";
+status="";
+objects=[];
+function preload(){
+  img = loadImage('dog_cat.jpg');
+}
+
+function setup() {
+  canvas = createCanvas(640, 420);
+  canvas.center();
+  objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML="status: detecting Objects";
+
+}
+
+
+function draw() {
+  image(img, 0, 0, 640, 420);
+  if(status != ""){
+    for(i=0;i<objects.length;i++){
+      document.getElementById("status").innerHTML="Status:object detector";
+      fill("#FF0000");
+          percent=floor(objects[i].confidence*100);
+          text(objects[i].label + " "+percent+"%",objects[i].x,object[i].y);
+          noFill();
+          stroke("#FF0000");
+          rect(objects[i].x,objects[i].y,objects[i].width,objects[i]);
+    }
+  }
+  fill("#FF0000");
+  text("Dog", 45, 75);
+  noFill();
+  stroke("#FF0000");
+  rect(30, 60, 450, 350 );
+fill("#FF0000");
+text("Cat" , 320,120);
+noFill();
+stroke("#FF0000");
+rect(300,90,270,320);
+
+}
+function modelLoaded(){
+  console.log("model loaded!");
+  status=true;
+  objectDetector.objectDetector(img, gotResult);
+}
+function gotResult(error,results){
+if(error){
+  console.log(error);
+objects=results;
+}
+else{
+  console.log(results);
+}
+}
